f2e4971101cd3b2e3f5a3ac2f8823e88d7e36039
Shivansh Saxena <i.shivanshhh@gmail.com>
2026-07-05 23:43:11 +0530

    page-id grasping from params fixed

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -2,9 +2,7 @@ export default function UserProfilePage({params}: any){
     return (
         <div>
             <h1>Profile-Page</h1>
-            <p>Profile page of user
-            </p>
-            <span>{params.id}</span>
+            <p>Profile page of user: {params.id}</p>
         </div>
     )
 }
